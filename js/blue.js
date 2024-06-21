9479d23e94a9c622befee7fe14a07ccf19d6d3fd
Adam <chindesaurus@users.noreply.github.com>
2024-06-21 12:00:36 -0400

    Add files via upload

--- js/blue.js
+++ js/blue.js
@@ -1,14 +1,5 @@
 var blue = [
-[	37.999333	, -122.344866, 'Amazon Whole Foods Retail Food Distribution Center', '6035 Giant Highway, Richmond, CA'],
-[	33.998738	, -118.224827, 'Amazon Whole Foods Retail Food Distribution Center', '5000 Pacific Boulevard, Vernon, CA'],
-[	39.758447	, -104.777801, 'Amazon Whole Foods Retail Food Distribution Center', '17900 East 32nd Avenue, Aurora, CO'],
-[	41.552848	, -72.873895, 'Amazon Whole Foods Retail Food Distribution Center', '400 East Johnson Avenue, Cheshire, CT'],
-[	34.102316	, -83.776839, 'Amazon Whole Foods Retail Food Distribution Center', '211 BDC Parkway, Braselton, GA'],
-[	41.696813	, -87.601391, 'Amazon Whole Foods Retail Food Distribution Center', '720 E 111th Street, Chicago, IL'],
-[	42.615673	, -70.650771, 'Amazon Whole Foods Retail Food Distribution Center', '15 Parker Street, Gloucester, MA'],
-[	38.91128	, -76.9029, 'Amazon Whole Foods Retail Food Distribution Center', '1555 Cabin Branch Drive, Landover, MD'],
-[	38.915929	, -76.902339, 'Amazon Whole Foods Retail Food Distribution Center', '1905 Clarkson Way, Hyattsville, MD'],
-[	30.253292	, -97.685622, 'Amazon Whole Foods Retail Food Distribution Center', '840 Interchange Boulevard, Austin, TX'],
-[	47.564473	, -122.338048, 'Amazon Whole Foods Retail Food Distribution Center', '4250 E Marginal Way South, Seattle, WA'],
-[	47.548082	, -122.333112, 'Amazon Whole Foods Retail Food Distribution Center', '5980 1st Avenue South, Seattle, WA'],
+[26.0078201488833, -80.36873234, 'Label 598', 'Pembroke Pines, FL 33027'],
+[26.1179119, -80.392026, 'Label 781', 'Weston, FL 33326'],
+[25.9104507, -80.3184946, 'Label 828', 'Miami Lakes, FL 33014'],
 ];
